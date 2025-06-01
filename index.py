7d4803014ba4bf2a1df4555c4eba65d971064bc0
imashvik <142769672+imashvik@users.noreply.github.com>
2025-06-01 08:00:48 -0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,22 @@
+import webbrowser
+print("welcome to my website opening program")
+a=input("do you want to see my websites.(yes/no)")
+if a=="yes":
+    b=input("which one.1 is imashvik main and about football.So if you want to select imashvik press 1.2 is about computers.So if you want to select about computers press 2.3 is about best game. so if you want to select best games 3")
+if b=="1":
+    webbrowser.open_new_tab("https://imashvik.github.io/")
+elif b=="2":
+      webbrowser.open_new_tab("https://imashvik.github.io/About-Computers/")
+else:
+      webbrowser.open_new_tab("https://imashvik.github.io/best-games/")
+c=input("do you want to see my websites again .(yes/no)")
+   
+    
+    
+    
+
+
+    
+
+
+
